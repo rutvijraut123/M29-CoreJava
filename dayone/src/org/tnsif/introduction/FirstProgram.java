7f5b8a963a94c37e6a1927e4da21393a2609a2d7
rutvij <rutvij@192.168.0.111>
2023-04-17 19:09:44 +0530

    first commit

---
+++ dayone/src/org/tnsif/introduction/FirstProgram.java
@@ -0,0 +1,10 @@
+package org.tnsif.introduction;
+//Program to demonstrate on simple java program
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
